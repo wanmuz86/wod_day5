db449825e6131a42a4fcab4245ffe73ffa0c66a1
Wan Muzaffar <wanmuz86@gmail.com>
2017-07-28 17:02:10 +0800

    feedback

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function staircase(n){
 
     for(var i=0; i<=5; i++){
-
+//wrong..
+        
+        //staircase should be
+        //****0
+        //***00
+        //**000
+        //*0000
+        //00000
         for(var j=0; j<i; j++){
             document.write("*");
         }
